0dcce415d00676657a4bfb544c1002a5e0814861
Kwon Sang <kwonsg514@gmail.com>
2023-12-31 01:54:29 +0900

    setting django-debugtoolbar

--- src/config/urls.py
+++ src/config/urls.py
@@ -5,7 +5,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Debug-toolbar setting
+    import debug_toolbar
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
