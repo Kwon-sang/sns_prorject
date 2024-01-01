32197b7b56d5a0ce440be7f09972cae57a62025e
Kwon Sang <kwonsg514@gmail.com>
2024-01-01 18:03:34 +0900

    feat: login, logout and logout then login

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.signup, name='sign_up')
+    path('signup/', views.signup, name='sign_up'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ]
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,8 +1,17 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
-from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView, logout_then_login
+from django.contrib.auth import login as auth_login
 
 from .forms import UserSignupForm
+
+
+login = LoginView.as_view(template_name='accounts/login_form.html')
+
+
+def logout(request):
+    messages.success(request, '로그아웃 하였습니다.')
+    return logout_then_login(request)
 
 
 def signup(request):
@@ -10,8 +19,9 @@ def signup(request):
         form = UserSignupForm(data=request.POST)
         if form.is_valid():
             user = form.save()
+            auth_login(request, user=user)
             messages.success(request, '회원가입을 환영합니다.')
-            next_url = request.get('next', '/')
+            next_url = request.GET.get('next', '/')
             return redirect(next_url)
     else:
         form = UserSignupForm()
